672d835186604929306e9945449383135ad3205c
PEZ <pez@pezius.com>
2026-03-20 15:13:42 +0100

    WIP: Add option level completion

--- completions.zsh
+++ completions.zsh
@@ -1,5 +1,13 @@
-_bbg_tasks() {
-    local matches=(`bbg tasks |tail -n +3 |cut -f1 -d ' '`)
-    compadd -a matches
+_bbg() {
+    if (( CURRENT == 2 )); then
+        local tasks=(`bbg tasks | tail -n +3 | cut -f1 -d ' '`)
+        compadd -a tasks
+    else
+        local task="${words[2]}"
+        local opts=(`bbg tasks | grep "^${task} " | grep -oE '\-\-[a-z-]+'`)
+        if (( ${#opts} )); then
+            compadd -a opts
+        fi
+    fi
 }
-compdef _bbg_tasks bbg
+compdef _bbg bbg
