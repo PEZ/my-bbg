9e587a6cd0e1701b6be7bd8d120e1c481e367dce
PEZ <pez@pezius.com>
2026-03-31 23:53:40 +0200

    Make bb loc accept file args

--- scripts/loc.clj
+++ scripts/loc.clj
@@ -1,11 +1,13 @@
 (ns loc
   (:require [babashka.process :as p]))
 
-(defn count! []
-  (p/shell
-   "cloc"
-   "--vcs=git"
-   "--exclude-lang=Markdown"
-   "--exclude-dir=build,playwright-report,test-results"
-   "--not-match-f=\\.mjs$"
-   "."))
+(defn count!
+  ([] (count! nil))
+  ([paths]
+   (apply p/shell
+          "cloc"
+          "--vcs=git"
+          "--exclude-lang=Markdown"
+          "--exclude-dir=build,playwright-report,test-results"
+          "--not-match-f=\\.mjs$"
+          (if (seq paths) paths ["."]))))
